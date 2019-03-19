70a7d6461a5890ad6517906e59a872553a91bf0f
Eduardo Rosario <erosario@nodrix.com>
2019-03-19 11:33:48 -0400

    New features
    - Required fields validation exception
    - Persist function per instanced model
    - Create context per schema model

--- source/common/utils.js
+++ source/common/utils.js
@@ -4,9 +4,41 @@ const path = require('path');
 
 const utils = {
 	data : {
-		copy : function (obj) {
+		copy     : function (obj) {
 			if (obj) {
 				return JSON.parse(JSON.stringify(obj));
+			} else {
+				return null;
+			}
+		},
+		validate : function (err, inner) {
+			if (err && err.name === 'ValidationError') {
+				var fields = [];
+				var message = '';
+
+				for (var field in err.errors) {
+
+					var item = err.errors[field];
+					var fName = item.message;
+
+					var inx = fName.indexOf('/');
+					if (inx > -1) {
+						var index = parseInt(fName.substring(0, inx));
+						fName = fName.substring(inx + 1);
+						fields.push({
+							name  : field,
+							index : index
+						});
+					} else {
+						fields.push({
+							name  : fName,
+							index : -1
+						});
+					}
+					message += (message.length ? ', ' : '') + fName;
+				}
+
+				return inner.args(message).toError({ meta : { fields : fields } });
 			} else {
 				return null;
 			}
--- source/middleware/data.js
+++ source/middleware/data.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const utils = require('../common/utils');
 
 module.exports = function (service, models, context) {
 	let _self = this;
@@ -10,11 +11,35 @@ module.exports = function (service, models, context) {
 	_self.find = {};
 	_self.list = {};
 	_self.retrieve = {};
+	_self.create = {};
+
+	let setupModel = function (model) {
+		if (model !== undefined && model !== null) {
+			model.persist = function () {
+				return new Promise(function (resolve, reject) {
+					model.save().then(function (data) {
+						resolve(data);
+					}).catch(function (err) {
+						var valErr = utils.data.validate(err, context.log.exception.missing_fields);
+						if (valErr) {
+							context.log.exception.data_validation_exception.args(valErr).throw(context);
+						} else {
+							_self.service.log.exception.data_exception.args(err).throw(context);
+						}
+						if (reject) {
+							reject(err);
+						}
+					});
+				});
+			};
+		}
+	};
 
 	let FindContext = function (model) {
 		return function (params) {
 			return new Promise(function (resolve, reject) {
 				model.findOne(params).then(function (data) {
+					setupModel(data);
 					resolve(data);
 				}).catch(function (err) {
 					_self.service.log.exception.data_exception.args(err).throw(context);
@@ -30,6 +55,11 @@ module.exports = function (service, models, context) {
 		return function (params) {
 			return new Promise(function (resolve, reject) {
 				model.find(params).then(function (data) {
+					if (data instanceof Array) {
+						for (var i = 0; i < data.length; i++) {
+							setupModel(data[i]);
+						}
+					}
 					resolve(data);
 				}).catch(function (err) {
 					_self.service.log.exception.data_exception.args(err).throw(context);
@@ -45,6 +75,7 @@ module.exports = function (service, models, context) {
 		return function (objectid) {
 			return new Promise(function (resolve, reject) {
 				model.findById(objectid).then(function (data) {
+					setupModel(data);
 					resolve(data);
 				}).catch(function (err) {
 					_self.service.log.exception.data_exception.args(err).throw(context);
@@ -56,12 +87,21 @@ module.exports = function (service, models, context) {
 		}
 	};
 
+	let CreateContext = function (model, name) {
+		return function () {
+			let newModel = new _models.list[name]();
+			setupModel(newModel);
+			return newModel;
+		}
+	};
+
 	if (_models && _models.list) {
 		for (var key in _models.list) {
 			let model = _models.list[key];
 			_self.find[key] = new FindContext(model);
 			_self.list[key] = new ListContext(model);
 			_self.retrieve[key] = new RetrieveContext(model);
+			_self.create[key] = new CreateContext(model, key);
 		}
 	}
 
